d5ebe69283105993580188c3dfc7e586bd48f75c
naxtre <naxtre1@gmail.com>
2021-01-15 17:53:27 +0530

    toast meassage

--- mylibrary/src/main/java/com/example/mylibrary/New.java
+++ mylibrary/src/main/java/com/example/mylibrary/New.java
@@ -1,11 +1,21 @@
 package com.example.mylibrary;
 
+import android.content.Context;
 import android.util.Log;
+import android.widget.Toast;
 
 public class New {
-    private static final String TaG="Testing Purpose";
-    public static void  d (String message)
-    {
-        Log.d("TaG",message);
-    }
+    public static void s(Context c, String message){
+
+    Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
 }
+}
+
+//[![](https://jitpack.io/v/monalisanaxtre/mylibrary.svg)](https://jitpack.io/#monalisanaxtre/mylibrary)
+// implementation 'com.github.monalisanaxtre:mylibrary:0.1.0'
+//public static void s(Context c,String message){
+//
+//    Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+//
+//}
