cb928e0fa27df35c6d7037564f49cc2e71edfd7e
naxtre <naxtre1@gmail.com>
2021-01-15 17:15:16 +0530

    initial commit

---
+++ mylibrary/src/main/java/com/example/mylibrary/New.java
@@ -0,0 +1,11 @@
+package com.example.mylibrary;
+
+import android.util.Log;
+
+public class New {
+    private static final String TaG="Testing Purpose";
+    public static void  d (String message)
+    {
+        Log.d("TaG",message);
+    }
+}
